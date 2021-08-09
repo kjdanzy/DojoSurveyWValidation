a282c0840f49b5abe610cec46221696054971edb
kjdanzy <64291380+kjdanzy@users.noreply.github.com>
2021-08-09 13:18:14 -0500

    Dojo Survey With Validation

---
+++ Models/InfoEntry.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyWValidation.Models
+{
+
+    public class InfoEntry
+    {
+        [Required(ErrorMessage ="Your name is required!")]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [MinLength(20, ErrorMessage = "Your comment must be at least 20 characteres.")]        
+        public string Comment { get; set; }
+
+    }
+
+}
